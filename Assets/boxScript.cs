9ab0e2202399f8cb58fd17e8117f82baeb7c8506
Sirazum Munira Tisha <tisha.ruet09@gmail.com>
2023-10-24 13:52:04 -0500

    small changes

--- Assets/boxScript.cs
+++ Assets/boxScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI
 
 public class boxScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    Rigidbody2D rd;
+    public static int count;
     void Start()
     {
-        
+        count = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void onTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player")){
+            gameObject.SetActive(false);
+            count++;
+        }
     }
 }
